77db48a9962fa15f60380377ffff2f6d254109cb
Brandon Boudreaux <hf3209@wayne.edu>
2022-11-24 00:27:20 -0500

    Can use any size

--- main.c
+++ main.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char* convert(const char *file_name){
+typedef struct {
+	unsigned char** pages;
+	int size;
+} PixelData;
+
+//Free struct
+void free_pixel_data(PixelData *p){
+	unsigned char** pages = (*p).pages;
+
+	for(int s = 0; s < (*p).size; s++){
+		free(pages[s]);
+	}
+
+	free(p);
+}
+
+PixelData* convert(const char *file_name){
 	FILE *fp = fopen(file_name, "rb");
 	if(!fp){
 		printf("This is not a valid file..\n");
+		printf("Usage: pngtby [file]\n");
 		return NULL;
 	}
 
@@ -15,6 +32,7 @@ unsigned char* convert(const char *file_name){
 	
 	if(png_sig_cmp(header, 0, 8)){
 		printf("This is not a PNG!!");
+		printf("Usage: pngtby [file]\n");
 		fclose(fp);
 		fp = NULL;
 		return NULL;
@@ -44,33 +62,61 @@ unsigned char* convert(const char *file_name){
 
 	png_bytepp row_ptr = png_get_rows(png_ptr, info_ptr);
 
-	unsigned char *data_t = malloc(5*sizeof(unsigned char));
+	int width = png_get_image_width(png_ptr, info_ptr);
+	int height = png_get_image_height(png_ptr, info_ptr); 
 
-	//Height
-	for (int height = 0; height < 8; height++){
-		png_bytep row_data_ptr = row_ptr[height];
+	if(width > 64 || height > 128){
 
-		//Width
-		for(int x = 0; x < 5; x++){
-			//R G B A per pixel
-			//Get the address of the pointer at our x pixel.
-			png_bytep png_pix_ptr = &row_data_ptr[x*4];
+		printf("This file is too large for the display!\n");
 
-			printf("[%d, %d, %d, %d]\n", png_pix_ptr[0], png_pix_ptr[1], png_pix_ptr[2], png_pix_ptr[3]);
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
 
-			if (png_pix_ptr[3] != 0){
-				//Write one bit
-				data_t[x] = data_t[x] | 0x01 << height;
+		fclose(fp);
+
+		fp=NULL;
+
+		return NULL;
+	}
+
+	int size;
+	if(height%2 == 0 && height >= 8){
+		size = height/8;
+	}else if(height < 8){
+		size = 1;
+	}else if(height%2 != 0 && height >= 8){
+		//Truncate and add 1
+		size = height/8 + 1;
+	}
+
+	unsigned char **pages = malloc(size * sizeof(unsigned char*));
+
+	/*unsigned char *data_t = malloc(width*sizeof(unsigned char));*/
+
+	for(int s = 0; s < size; s++){
+		pages[s] = malloc(width*sizeof(unsigned char));
+
+		//Height
+		for (int h = 0; h < height; h++){
+			png_bytep row_data_ptr = row_ptr[h];
+
+			//Width
+			for(int x = 0; x < width; x++){
+				//R G B A per pixel
+				//Get the address of the pointer at our x pixel.
+				png_bytep png_pix_ptr = &row_data_ptr[x*4];
+
+				if (png_pix_ptr[3] != 0){
+					//Write one bit
+					pages[s][x] = pages[s][x] | 0x01 << h;
+				}
 			}
 		}
 	}
 
+	PixelData *p = malloc(sizeof(PixelData));  
 
-	//Verification
-	for(int i = 0; i < 5; i++){
-		printf("%#02x\n", data_t[i]);
-		printf("%#02x\n", (unsigned char)~data_t[i]);
-	}
+	(*p).pages = pages;
+	(*p).size = size;
 
 	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
 
@@ -78,25 +124,36 @@ unsigned char* convert(const char *file_name){
 
 	fp=NULL;
 
-	return data_t;
+	return p; 
 }
 
 int main(int argc, char *argv[]){
 
-	if(argc == 1){
-		printf("Usage: pngtby [width] [height] [file]\n");
+	if(argc == 1 || argc < 2){
+		printf("Usage: pngtby [file]\n");
 		return 0;
 	}
 
-	printf("%s\n", argv[1]);
-	unsigned char* data = convert(argv[1]);
-	if(!data){
+	char *file = argv[1];
+
+	PixelData *p_data = convert(file);
+	if(!p_data){
 		return -1;
 	}
 
 	printf("Output of bytes: \n");
-	printf("%#02x, %#02x, %#02x, %#02x, %#02x,\n", data[0], data[1], data[2], data[3], data[4]);
+	for(int p = 0; p <  (*p_data).size; p++){
+		printf("{ ");
+		unsigned char* page = (*p_data).pages[p];
 
-	free(data);
+		for(int d = 0; d < sizeof(page)-1; d++){
+			if(page[d] == 0){
+				printf("0x00, ");
+				continue;
+			}
+			printf("%#02x, ", page[d]);
+		}
+		printf("},\n");
+	}
+	free_pixel_data(p_data);
 }
-
